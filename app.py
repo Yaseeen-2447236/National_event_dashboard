7f8fb17ab5232a5c6ce72a2ebf6692ac88c403f8
yaseenahmed1407 <yaseenahmed1407@gmail.com>
2025-04-03 12:35:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 import seaborn as sns
 from wordcloud import WordCloud
 from sklearn.feature_extraction.text import TfidfVectorizer
